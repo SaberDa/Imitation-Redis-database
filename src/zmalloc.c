e139f3e67a9b172a92083884627f23655c07488f
saberda <630991493@qq.com>
2020-05-19 10:12:27 -0400

    add zcalloc funtion

--- src/zmalloc.c
+++ src/zmalloc.c
@@ -103,5 +103,18 @@ void *zmalloc(size_t size) {
 }
 
 void *zcalloc(size_t size) {
-
+    void *ptr = calloc(1, size + PREFIX_SIZE);
+    if (!ptr) {
+        zmalloc_oom_handler(size);
+    }
+#ifdef HAVE_MALLOC_SIZE
+    update_zmalloc_stat_alloc(size + PREFIX_SIZE);
+    return ptr;
+#else
+    *((size_t*)ptr) = size;
+    update_zmalloc_stat_alloc(size + PREFIX_SIZE);
+    return (char*)ptr + PREFIX_SIZE;
+#endif
 }
+
+
