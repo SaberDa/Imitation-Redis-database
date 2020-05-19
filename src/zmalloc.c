e74036af8fe41256e20e40f08273259281269ffb
saberda <630991493@qq.com>
2020-05-19 10:18:14 -0400

    add zrealloc function

--- src/zmalloc.c
+++ src/zmalloc.c
@@ -117,4 +117,38 @@ void *zcalloc(size_t size) {
 #endif
 }
 
+void *zrealloc(void *ptr, size_t size) {
+#ifndef HAVE_MALLOC_SIZE
+    void *realptr;
+#endif
+    size_t oldsize;
+    void *newptr;
 
+    if (ptr == NULL) {
+        return zmalloc(size);
+    }
+
+#ifdef HAVE_MALLOC_SIZE
+    oldsize = zmalloc_size(ptr);
+    newptr = realloc(ptr, size);
+    if (!newptr) {
+        zmalloc_oom_handler(size);
+    }
+
+    update_zmalloc_stat_free(oldsize);
+    update_zmalloc_stat_alloc(zmalloc_size(newptr));
+    return newptr;
+#else 
+    realptr = (char*)ptr - PREFIX_SIZE;
+    oldsize = *((size_t*)realptr);
+    newptr = realloc(realptr, size + PREFIX_SIZE);
+    if (!newptr) {
+        zmalloc_oom_handler(size);
+    }
+
+    *((size_t*)newptr) = size;
+    update_zmalloc_stat_free(oldsize);
+    update_zmalloc_stat_alloc(size);
+    return (char*)newptr +PREFIX_SIZE;
+#endif
+}
