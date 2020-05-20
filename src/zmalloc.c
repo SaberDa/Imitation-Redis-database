6bac034344868e67a9c35dcdc55308117be1551f
saberda <630991493@qq.com>
2020-05-20 10:53:42 -0400

    add comments

--- src/zmalloc.c
+++ src/zmalloc.c
@@ -55,6 +55,11 @@ void zlibc_free(void *ptr) {
 
 #endif
 
+// 原子加操作
+// 内存状态统计函数
+// 首先将n调整为sizeof(long)的整数倍
+// 如果使用了线程安全模式，则调用原子操作(+)来更新已知内存
+// 若不考虑线程安全，则直接更新已知内存
 #define update_zmalloc_stat_alloc(__n) do { \
     size_t _n = (__n); \
     if (_n&(sizeof(long)-1)) _n += sizeof(long)-(_n&(sizeof(long)-1)); \
@@ -65,6 +70,11 @@ void zlibc_free(void *ptr) {
     } \
 } while(0)
 
+// 原子减函数
+// 内存状态统计函数
+// 先将内存大小调整为sizeof(long)的整数倍
+// 若开启了线程安全模式，则调用原子操作(-)来更新已知内存
+// 若不考虑线程安全，则直接更新已知内存
 #define update_zmalloc_stat_free(__n) do { \
     size_t _n = (__n); \
     if (_n&(sizeof(long)-1)) _n += sizeof(long)-(_n&(sizeof(long)-1)); \
@@ -76,35 +86,45 @@ void zlibc_free(void *ptr) {
 } while(0)
 
 
-
+// 已使用内存大小
 static size_t used_memory = 0;
+// 线程安全模式状态
 static int zmalloc_thread_safe = 0;
+// 服务器
 pthread_mutex_t used_memory_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+// 内存异常处理函数
 static void zmalloc_default_oom(size_t size) {
     fprintf(stderr, "zmalloc: Out of memory trying to allocate %zu bytes\n", size);
     fflush(stderr);
-    about();
+    about();        // 中断退出
 }
 
 static void(*zmalloc_oom_handler)(size_t) = zmalloc_default_oom;
 
 void *zmalloc(size_t size) {
+    // 调用malloc函数进行内存申请
+    // 多申请的PrREFIX_SIZE大小的内存用于记录该段内存的大小
     void *ptr = malloc(size + PREFIX_SIZE);
+    // 如果ptr为空，则调用异常处理函数
     if (!ptr) zmalloc_oom_handler(size);
 #ifdef HAVE_MALLOC_SIZE
     update_zmalloc_stat_alloc(zmalloc_size(ptr));
     return ptr;
 #else 
+    // 内存统计
     *((size_t*)ptr) = size;
+    // 更新used_memory
     update_zmalloc_stat_alloc(size + PREFIX_SIZE);
     return (char*)ptr+PREFIX_SIZE;
 #endif
 }
 
+// 调用calloc申请内存
 void *zcalloc(size_t size) {
     void *ptr = calloc(1, size + PREFIX_SIZE);
     if (!ptr) {
+        // 异常处理函数
         zmalloc_oom_handler(size);
     }
 #ifdef HAVE_MALLOC_SIZE
@@ -117,6 +137,8 @@ void *zcalloc(size_t size) {
 #endif
 }
 
+// 内存调整函数
+// 用于调整已申请内存的大小，本质调用了系统的recalloc()
 void *zrealloc(void *ptr, size_t size) {
 #ifndef HAVE_MALLOC_SIZE
     void *realptr;
@@ -124,6 +146,7 @@ void *zrealloc(void *ptr, size_t size) {
     size_t oldsize;
     void *newptr;
 
+    // 若ptr为空，则直接退出
     if (ptr == NULL) {
         return zmalloc(size);
     }
@@ -139,14 +162,18 @@ void *zrealloc(void *ptr, size_t size) {
     update_zmalloc_stat_alloc(zmalloc_size(newptr));
     return newptr;
 #else 
+    // 找到内存真正的起始位置
     realptr = (char*)ptr - PREFIX_SIZE;
     oldsize = *((size_t*)realptr);
+    // 调用realloc()函数
     newptr = realloc(realptr, size + PREFIX_SIZE);
     if (!newptr) {
         zmalloc_oom_handler(size);
     }
-
+    // 内存统计
     *((size_t*)newptr) = size;
+    // 先减去原先已使用内存大小
+    // 然后再加上调整后的大小
     update_zmalloc_stat_free(oldsize);
     update_zmalloc_stat_alloc(size);
     return (char*)newptr +PREFIX_SIZE;
@@ -173,12 +200,15 @@ size_t zmalloc_size(void *ptr) {
 }
 #endif
 
+// 内存释放函数
+// 调用系统free()函数
 void zfree(void *ptr) {
 #ifndef HAVE_MALLOC_SIZE
     void *realptr;
     size_t oldsize;
 #endif
 
+    // 为空直接返回
     if (ptr == NULL) {
         return;
     }
@@ -187,28 +217,37 @@ void zfree(void *ptr) {
     update_zmalloc_stat_free(zmalloc_size(ptr));
     free(ptr);
 #else 
+    // 找到该段内存的起始位置
     realptr = (char*)ptr - PREFIX_SIZE;
     oldsize = *((size_t*)realptr);
+    // 更新used_memory
     update_zmalloc_stat_free(oldsize + PREFIX_SIZE);
+    // 释放内存
     free(realptr);
 #endif
 }
 
+// 字符串复制方法
 char *zstrdup(const char *s) {
+    // 开辟一段新内存
     size_t l = strlen(s) + 1;
     char *p = zmalloc(l);
 
-    memccpy(p, s, l);
+    // 复制字符串
+    memcpy(p, s, l);
     return p;
 }
 
+// 获取已知内存
 size_t zmalloc_used_memory(void) {
     size_t um;
 
     if (zmalloc_thread_safe) {
 #ifdef HAVE_ATOMIC
+// 使用GCC提供的原子操作
         um = __sync_add_and_fetch(&used_memory, 0);
 #else 
+// 若不支持原子操作，则使用线程锁
         pthread_mutex_lock(&used_memory_mutex);
         um = used_memory;
         pthread_mutex_unlock(&used_memory_mutex);
@@ -220,10 +259,13 @@ size_t zmalloc_used_memory(void) {
     return um;
 }
 
+// 开启线程安全
 void zmalloc_enable_thread_safemess(void) {
     zmalloc_thread_safe = 1;
 }
 
+// 允许自行设定异常处理函数
+// 可绑定自定义的异常处理函数
 void zmalloc_set_oom_handler(void (*oom_handler)(size_t)) {
     zmalloc_oom_handler = oom_handler;
 }
