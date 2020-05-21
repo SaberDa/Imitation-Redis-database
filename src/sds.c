92b7a004dd2e60cc4ebfd108bdc6ba204f948f98
saberda <630991493@qq.com>
2020-05-21 10:06:36 -0400

    add sdsnewlen comment

--- src/sds.c
+++ src/sds.c
@@ -32,25 +32,36 @@
 */
 sds sdsnewlen(const void *init, size_t initlen) {
     struct sdshdr *sh;
-
+    
+    // 根据是否有初始化内容，选择适当所分配的内存
+    // T = O(n)
     if (init) {
+        // zmalloc 不初始化所分配的内存
         sh = zmalloc(sizeof(struct sdshdr) + initlen + 1);
     } else {
+        // zcalloc 将分配的内存全部初始化为0
         sh = zcalloc(sizeof(struct sdshdr) + initlen + 1);
     }
 
+    // 内存分配失败
     if (sh == NULL) {
         return NULL;
     }
 
+    // 设置初始化长度
     sh->len = initlen;
+    // 新sds不预留任何空间
     sh->free = 0;
 
+    // 如果有指定初始化内容，将他们复制到 sdshdr 的 buf 中
+    // T = O(n)
     if (initlen && init) {
         memcpy(sh->buf, init, initlen);
     }
 
+    // 以  \0 结尾
     sh->buf[initlen] = '\0';
 
+    // 返回 buf 部分，而不是整个 sdshdr
     return (char*)sh->buf;
 }
