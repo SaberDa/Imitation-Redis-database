2a23f7310dc21186786cc3516cd2c4d501b51403
saberda <630991493@qq.com>
2020-05-25 14:12:32 -0400

    add function sdsll2str

--- src/sds.c
+++ src/sds.c
@@ -534,3 +534,45 @@ sds sdscpylen(sds s, const char *t, size_t len) {
 sds sdscpy(sds s, const char *t) {
     return sdscpylen(s, t, strlen(t));
 }
+
+/*
+ * Helper for sdscatlonglogn() doing the actual number -> string conversion
+ * 's' must point to a string with room for at least SDS_LLSTR_SIZE bytes
+ * 
+ * The function returns the length of the null-terminated string representation 
+ * stored at 's'
+ */
+#define SDS_LLSTR_SIZE 21
+int sdsll2str(char *s, long long value) {
+    char *p;
+    char aux;
+    unsigned long long v;
+    size_t l;
+
+    // Generate the string representation, this method produces
+    // an reversed string
+    v = (value < 0) ? -value : value;
+    p = s;
+    do {
+        *p++ = '0' + (v % 10);
+        v /= 10;
+    } while (v);
+    if (value < 0) {
+        *p++ = '-';
+    }
+
+    // Compute length and add null term
+    l = p - s;
+    *p = '\0';
+
+    // Reverse the string
+    p--;
+    while (s < p) {
+        aux = *s;
+        *s = *p;
+        *p = aux;
+        s++;
+        p--;
+    }
+    return l;
+}
