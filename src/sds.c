2e73088519fec438fe758af4b7a2a22ca085fd09
saberda <630991493@qq.com>
2020-05-30 13:31:01 -0400

    add function sdscatprintf()

--- src/sds.c
+++ src/sds.c
@@ -679,3 +679,33 @@ sds sdscatvprintf(sds s, const char *fmt, va_list ap) {
     }
     return t;
 }
+
+/*
+ * 打印任意数量个字符串，并将这些字符串追加到给定 sds 的末尾
+ * 
+ * T = O(N^2)
+ * 
+ * Append to the sds string 's' a string obtained using printf-alike format
+ * specifier.
+ * 
+ * After the call, the modified sds string is no longer valid and all the 
+ * references must be substituted with the new pointer returned by the call.
+ * 
+ * Example:
+ * s = sdsempty("Sum is: ");
+ * s = sdscatprintf(s, "%d + &d = %d", a, b, a+b);
+ * 
+ * Often you need to create a string from scratch with the printf-alike
+ * format, When this is need, just use sdsempty() as the target string:
+ * 
+ * s = sdscatprintf(sdsempty(), "... your format ...", args);
+*/
+sds sdscatprintf(sds s, const char *fmt, ...) {
+    va_list ap;
+    char *t;
+    va_start(ap, fmt);
+    // T = O(N)
+    t = sdscatvprintf(s, fmt, ap);
+    va_end(ap);
+    return t;
+}
