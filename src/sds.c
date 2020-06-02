fc41f5c5eb819ce3964e685ebb8067dd66aba644
saberda <630991493@qq.com>
2020-06-02 12:05:15 -0400

    add function sdscatfmt()

--- src/sds.c
+++ src/sds.c
@@ -709,3 +709,118 @@ sds sdscatprintf(sds s, const char *fmt, ...) {
     va_end(ap);
     return t;
 }
+
+/*
+ * This function is similar to sdscatprintf, but much faster as it does
+ * not rely on sprintf() family functions implemented by the libc that 
+ * are often very slow. Moreover directly handling the sds string as new 
+ * data is concatenated provides a performance improvement.
+ * 
+ * However, this function only handles an incompatible subset of 
+ * printf-alike format specifiers
+*/
+sds sdscatfmt(sds s, char const *fmt, ...) {
+    struct sdshdr* sh = (void*) (s - sizeof(struct sdshdr));
+    size_t initlen = sdslen(s);
+    const char *f = fmt;
+    int i;
+    va_list ap;
+
+    va_start(ap, fmt);
+    // Next format specifier byte to process
+    f = fmt;
+    // Position of the next byte to write to dest str
+    i = initlen;
+    while (*f) {
+        char next;
+        char *str;
+        size_t l;
+        long long num;
+        unsigned long long unum;
+
+        // Make sure there  is always space for at least 1 char
+        if (sh->free == 0) {
+            s = sdsMakeRoomFor(s, l);
+            sh = (void*) (s - sizeof(struct sdshdr));
+        }
+
+        switch(*f) {
+            case '%' :
+                next = *(f + 1);
+                f++;
+                switch(next) {
+                    case 's':
+                    case 'S':
+                        str = va_arg(ap, char*);
+                        l = (next == 's') ? strlen(s) : sdslen(str);
+                        if (sh->free < l) {
+                            s = sdsMakeRoomFor(s, l);
+                            sh = (void*) (s - sizeof(struct sdshdr));
+                        }
+                        memcpy(s + i, str, l);
+                        sh->len += l;
+                        sh->free -= l;
+                        i += l;
+                        break;
+                    case 'i':
+                    case 'I':
+                        if (next == 'i') {
+                            num = va_arg(ap, int);
+                        } else {
+                            num = va_arg(ap, long long);
+                        }
+                        {
+                            char buf[SDS_LLSTR_SIZE];
+                            l = sdsll2str(buf, num);
+                            if (sh->free < l) {
+                                s = sdsMakeRoomFor(s, l);
+                                sh = (void*) (s - (sizeof(struct sdshdr)));
+                            }
+                            memcpy(s+i, buf, l);
+                            sh->len += l;
+                            sh->free -= l;
+                            i += l;
+                        }
+                        break;
+                    case 'u':
+                    case 'U':
+                        if (next == 'u') {
+                            unum = va_arg(ap, unsigned int);
+                        } else {
+                            unum = va_arg(ap, unsigned long long);
+                        }
+                        {
+                            char buf[SDS_LLSTR_SIZE];
+                            l = sdsll2str(buf, unum);
+                            if (sh->free < l) {
+                                s = sdsMakeRoomFor(s, l);
+                                sh = (void*) (s - (sizeof(struct sdshdr)));
+                            }
+                            memcpy(s+i, buf, l);
+                            sh->len += l;
+                            sh->free -= l;
+                            i += l;
+                        }
+                        break;
+                    default:
+                        // Handle %% and generally %<unknown>
+                        s[i++] = next;
+                        sh->len += 1;
+                        sh->free -= 1;
+                        break;
+                }
+                break;
+            default:
+                s[i++] = *f;
+                sh->len += 1;
+                sh->free -= 1;
+                break;
+        }
+        f++;
+    }
+    va_end(ap);
+
+    // Add null-term
+    s[i] = '\0';
+    return s;
+}
