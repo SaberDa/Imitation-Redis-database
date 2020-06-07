3537a314c57a6255bbd689ae5c97cb54f7ce5f29
saberda <630991493@qq.com>
2020-06-06 22:03:04 -0400

    add function hex_digit_to_int()

--- src/sds.c
+++ src/sds.c
@@ -1170,3 +1170,33 @@ int is_hex_digit(char c) {
     return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') ||
            (c >= 'A' && c <= 'F');
 }
+
+/*
+ * 将十六进制转换为十进制
+ * 
+ * Helper function for sdssplitargs() that converts a hex digit into 
+ * an interger from 0 to 15
+ * 
+ * T = O(1)
+*/
+int hex_digit_to_int(char c) {
+    switch(c) {
+        case '0' : return 0;
+        case '1' : return 1;
+        case '2' : return 2;
+        case '3' : return 3;
+        case '4' : return 4;
+        case '5' : return 5;
+        case '6' : return 6;
+        case '7' : return 7;
+        case '8' : return 8;
+        case '9' : return 9;
+        case 'a' : case 'A' : return 10;
+        case 'b' : case 'B' : return 11;
+        case 'c' : case 'C' : return 12;
+        case 'd' : case 'D' : return 13;
+        case 'e' : case 'E' : return 14;
+        case 'f' : case 'F' : return 15;
+        default: return 0;
+    }
+}
