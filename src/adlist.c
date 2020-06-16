ea19a3b99a6c9f71321bbc14cdffe0a62b682997
saberda <630991493@qq.com>
2020-06-16 15:09:35 -0400

    add function listCreate()

--- src/adlist.c
+++ src/adlist.c
@@ -2,7 +2,32 @@
 #include "adlist.h"
 #include "zmalloc.h"
 
-list *listCreate(void);
+/* Create a new list. The created list can be freed with
+ * AlFreeList(), but private value of evert node need to be
+ * freed by the user before to call AlFreeList().
+ * 
+ * Or error, NULL is returned. Otherwise the pointer to the new list.
+ */
+/* 创建一个新的链表
+ * 创建成功返回链表，失败返回 NULL
+ * T = O(1)
+ */
+list *listCreate(void) {
+    struct list *list;
+
+    // 分配内存
+    if ((list = zmalloc(sizeof(*list))) == NULL)
+        return NULL;
+    
+    // 初始化属性
+    list->head = list->tail = NULL;
+    list->len = 0;
+    list->dup = NULL;
+    list->free = NULL;
+    list->match = NULL;
+
+    return list;
+}
 void listRelease(list *list);
 list *listAddNodeHead(list *list, void *value);
 list *listAddNodeTail(list *list, void *value);
