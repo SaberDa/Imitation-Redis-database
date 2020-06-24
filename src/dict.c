fa94479aedc1a952a65a6703728b5f4fe9c10692
saberda <630991493@qq.com>
2020-06-24 11:04:21 -0400

    add function timeInMilliseconds()

--- src/dict.c
+++ src/dict.c
@@ -411,3 +411,15 @@ int dictRehash(dict *d, int n) {
     }
     return 1;
 }
+
+/*
+ * 返回以毫秒为单位的 UNIX 时间戳
+ * 
+ * T = O(1)
+*/
+long long timeInMilliseconds(void) {
+    struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+    return (((long long)tv.tv_sec) *1000) + (tv.tv_usec / 1000);
+}
