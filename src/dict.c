376a9e51c9f51c214daafae59a0bba10346377c4
saberda <630991493@qq.com>
2020-06-26 12:34:48 -0400

    add function dictFingerPrint()

--- src/dict.c
+++ src/dict.c
@@ -855,3 +855,49 @@ void *dictFetchValue(dict *d, const void *key) {
 
     return he ? dictGetVal(he) : NULL;
 }
+
+/*
+ * A fingerprint is a 64 bit number that represents the state of the dictionary
+ * at a given time, it's just a few dict properties XORed together.
+ * 
+ * When an unsafe iterator is initialized, we get the dict fingerprint, and 
+ * check the fingerprint again when the iterator is released.
+ * 
+ * If the two fingerprints are different it means that the user of the iterator
+ * preformed forbidden operations against the dictionary while iterating
+*/
+long long dictFingerPrint(dict *d) {
+    long long integers[6], hash = 0;
+    int j;
+
+    integers[0] = (long) d->ht[0].table;
+    integers[1] = d->ht[0].size;
+    integers[2] = d->ht[0].used;
+    integers[3] = (long) d->ht[1].table;
+    integers[4] = d->ht[1].size;
+    integers[5] = d->ht[1].used;
+
+    /*
+     * We hash N integers by summing every successive integer with the 
+     * integer hashing of previous sum.
+     * 
+     * Basically:
+     * Result = hash(hash(hash(int1) + int2) + int3)
+     * 
+     * This wat the same set of integers in a different order will (likely)
+     * hash to a different number
+    */
+   for (j = 0; j < 6; j++) {
+       hash += integers[j];
+       // For the hashing step we use Toma Wang's 64 bit integer hash
+       // hash = (hash << 21) - hash - 1
+       hash = (~hash) + (hash << 21);
+        hash = hash ^ (hash >> 24);
+        hash = (hash + (hash << 3)) + (hash << 8); // hash * 265
+        hash = hash ^ (hash >> 14);
+        hash = (hash + (hash << 2)) + (hash << 4); // hash * 21
+        hash = hash ^ (hash >> 28);
+        hash = hash + (hash << 31);
+   }
+   return hash;
+}
