9ef56f127cb0503ac53d65e4323db638ffafa958
saberda <630991493@qq.com>
2020-07-07 12:54:38 -0400

    add function _intsetGetEncoded()

--- src/intset.c
+++ src/intset.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// #include <stdint.h>
 
 #include "intset.h"
 #include "zmalloc.h"
@@ -31,4 +30,35 @@ static uint8_t _intsetValueEncoding(int64_t v) {
         return INTSET_ENC_INT32;
     else 
         return INTSET_ENC_INT16;
+}
+
+/* Return the value at pos, given an encoding */
+/*
+ * 根据给定的编码方式 enc，返回集合的底层数组在 pos 索引上的元素
+ * 
+ * T = O(1)
+*/
+static int64_t _intsetGetEncoded(intset *is, int pos, uint8_t enc) {
+    int64_t v64;
+    int32_t v32;
+    int16_t v16;
+
+    // ((ENCODING*)is->contents) 首先将数组转换回被编码的类型
+    // 然后 ((ENCODING*)is->contents) + pos 计算出元素在数组中的正确位置
+    // 之后 member(&vEnc, ..., sizeof(vEnc)) 再从数组中拷贝出正确数量的字节
+    // 如果有需要的话，memrevEncifbe(&vEnc) 会对拷贝出的字节进行大小端转换
+    // 最后将值返回
+    if (enc == INTSET_ENC_INT64) {
+        memcpy(&v64, ((int64_t*)is->contents) + pos, sizeof(v64));
+        memrev64ifbe(&v64);
+        return v64;
+    } else if (enc == INTSET_ENC_INT32) {
+        memcpy(&v32, ((int32_t*)is->contents) + pos, sizeof(v32));
+        memrev32ifbe(&v32);
+        return v32;
+    } else {
+        memcpy(&v16,((int16_t*)is->contents)+pos,sizeof(v16));
+        memrev16ifbe(&v16);
+        return v16;
+    }
 }
