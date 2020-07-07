14f8e5ec6426721fccd999c6c0b9d974fe6d21b0
saberda <630991493@qq.com>
2020-07-07 12:34:46 -0400

    add struct intset

--- src/intset.h
+++ src/intset.h
@@ -1,5 +1,15 @@
 #ifndef __INTSET_H__
 #define __INTSET_H__
 
+#include <stdint.h>
+
+typedef struct intset {
+    // 编码方式
+    uint32_t encoding;
+    // 集合包含的元素数量
+    uint32_t length;
+    // 保存元素的数组
+    int8_t contents[];
+} intset;
 
 #endif
