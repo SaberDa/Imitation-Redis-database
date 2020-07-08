fb5ea415631ea3955a2d8d28e94375d7aab26ae7
saberda <630991493@qq.com>
2020-07-08 13:00:14 -0400

    add struct zskiplistNode

--- src/zskiplist.h
+++ src/zskiplist.h
@@ -5,10 +5,32 @@
 
 #include <stdio.h>
 
+#include "redis.h"
+
 #ifndef __ZSKIPLIST_H__
 #define __ZSKIPLIST_H__
 
+/* ZSETs use a specialized version of Skiplists */
+// 跳跃表结点
+typedef struct zskiplistNode {
 
+    robj *obj;                      // 成员对象
 
+    double score;                   // 分值
+
+    struct zskiplistNode *backward; // 后退指针
+
+    // 层
+    struct zskiplistLevel {
+
+        struct zskiplistNode *forward; // 前进指针
+        
+        unsigned int span;             // 跨度
+
+    } level[];
+
+} zskiplistNode;
+
+// 跳跃表
 
 #endif
