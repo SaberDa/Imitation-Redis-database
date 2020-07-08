bd77a15aa85362cd945aac6d24f897042d4b37ad
saberda <630991493@qq.com>
2020-07-08 13:05:55 -0400

    add struct zskiplist

--- src/zskiplist.h
+++ src/zskiplist.h
@@ -32,5 +32,14 @@ typedef struct zskiplistNode {
 } zskiplistNode;
 
 // 跳跃表
+typedef struct zskiplist {
+
+    struct zskiplistNode *header, *tail; // 表头结点和表尾结点
+
+    unsigned long length;            // 表中结点的数量
+
+    int level;                       // 表中层数最大的结点的层数
+
+} zskiplist;
 
 #endif
